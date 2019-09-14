89c07eac33328d7c8e8bad4e1ddec92fbd57ddb2
venkatWebsite <venkatraman37@gmail.com>
2019-09-14 16:48:48 +0530

    ok

--- src/test/java/POM/BaseClass.java
+++ src/test/java/POM/BaseClass.java
@@ -8,8 +8,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-/**Author
-* venkat
+/**
+* Author-venkat
+* 
 * Date 14-09-2019
 */	
 
